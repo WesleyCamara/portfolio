0fe3809e6ed25d17ba2fce03a1a420d665e21f6c
WesleyCamara <wesleycamara@hotmail.com.br>
2020-03-30 18:32:45 -0300

    ajustes no scroll suave

--- js/script.js
+++ js/script.js
@@ -10,6 +10,34 @@ function maquina(elemento) {
     });
   }
 
-
   const titulo = document.querySelector('[data-anima="maquina"]');
   maquina(titulo);
+
+
+// scroll suave
+// --------------------------------------------------------------------
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    // section.scrollIntoView()
+
+
+
+    const topo = section.offsetTop;
+    window.scrollTo({
+        top: topo,
+        behavior: 'smooth'
+    })
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
+
+
+
+
