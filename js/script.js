70272d6f1e168c9f68347b3668ef241a34aa8bc5
Wesley Câmara <wesleycamara@hotmail.com.br>
2020-12-14 15:42:06 -0300

    teste pwa, display none

--- js/script.js
+++ js/script.js
@@ -1,40 +1,66 @@
 // efeito de maquina de escrever
 // --------------------------------------------------------------------
 function initMaquina() {
-    function maquina(elemento) {
-        const textoArray = elemento.innerHTML.split('');
-        elemento.innerHTML = '';
-        textoArray.forEach((letra, i) => {
-            setTimeout(() => elemento.innerHTML += letra, 75 * i);
-        });
-    }
+  function maquina(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => (elemento.innerHTML += letra), 75 * i);
+    });
+  }
 
-    const titulo = document.querySelector('[data-anima="maquina"]');
-    maquina(titulo);
+  const titulo = document.querySelector('[data-anima="maquina"]');
+  maquina(titulo);
 }
-initMaquina()
-
+initMaquina();
 
 // scroll suave
 // --------------------------------------------------------------------
 
 function initScrollSuave() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-    function scrollToSection(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
-    }
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
 
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection);
-    })
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
-initScrollSuave()
+initScrollSuave();
 
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
