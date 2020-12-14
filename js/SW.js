67879a997cba04c98a862fe61370657891b71438
Wesley Câmara <wesleycamara@hotmail.com.br>
2020-12-14 11:27:42 -0300

    SW 2

--- js/SW.js
+++ js/SW.js
@@ -10,3 +10,42 @@ if ('serviceWorker' in navigator) {
     .then((reg) => console.info('registered sw', reg))
     .catch((err) => console.error('error registering sw', err));
 }
+
+// Cache on install
+this.addEventListener('install', (event) => {
+  this.skipWaiting();
+
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    }),
+  );
+});
+
+// Clear cache on activate
+this.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName.startsWith('willian-justen-'))
+          .filter((cacheName) => cacheName !== staticCacheName)
+          .map((cacheName) => caches.delete(cacheName)),
+      );
+    }),
+  );
+});
+
+// Serve from Cache
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/offline/index.html');
+      }),
+  );
+});
